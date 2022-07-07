91dccb647018a2ca2fa6b0444c32b31545b3b515
mahesht-mrk <108858673+mahesht-mrk@users.noreply.github.com>
2022-07-07 15:07:00 +0530

    5thjuly task
    5thjuly task

---
+++ First.js
@@ -0,0 +1,14 @@
+function sum(){
+	console.log('Hello Mirketa welcome');
+}
+
+function printName(){
+	var fname = document.getElementById('firstname').value;
+	var Lname = document.getElementById('LanstName').value;
+	console.log('Hello' + fname,Lname);
+}
+
+function ptintSubtraction(){
+	var 
+	
+}
